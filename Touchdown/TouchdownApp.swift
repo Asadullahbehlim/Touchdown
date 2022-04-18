6bc951c5ae14b275d61fbf90d479889eb264d0ce
Asadullah Behlim <asadullahbehlim@gmail.com>
2022-04-18 14:17:52 +0530

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Asadullah Behlim on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
